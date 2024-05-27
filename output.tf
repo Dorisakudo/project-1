c470e75afbea0cca24afe71e8d555ac0e9fc9591
Dorisakudo <dorisakudo09@gmail.com>
2024-05-27 17:08:42 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,39 +1,22 @@
-output "vpc_id" {
-  description = "The VPC ID"
-  value       = module.vpc.vpc_id
-}
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
 
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
-}
-
-output "cluster_name" {
-  description = "EKS Cluster Name"
-  value       = module.eks.cluster_name
-}
-
-output "eks_cluster_endpoint" {
-  description = "EKS Cluster endpoint"
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
   value       = module.eks.cluster_endpoint
 }
 
-output "eks_cluster_security_group_id" {
-  description = "EKS Cluster security group id"
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
   value       = module.eks.cluster_security_group_id
 }
 
-output "node_groups" {
-  description = "List of EKS managed node group names"
-  value       = keys(module.eks.eks_managed_node_groups)
+output "region" {
+  description = "AWS region"
+  value       = var.region
 }
 
-output "ebs_csi_driver_role_arn" {
-  description = "IAM Role ARN for EBS CSI Driver"
-  value       = module.irsa_ebs_csi.iam_role_arn
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
 }
